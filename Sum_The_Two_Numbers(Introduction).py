08d1fe09a9f204aa9b64309d299f2dd05f332658
Bhargav Aloni <bmaloni@gmail.com>
2023-05-21 20:00:26 +0530

    commit message

---
+++ Sum_The_Two_Numbers(Introduction).py
@@ -0,0 +1,8 @@
+# ADDITION OF TWO NUMBERS
+
+x = int (input('Enter any number : '))
+print ('Enter another number :')
+y = input()
+sum = int(x) + int(y)
+print ('This operation will give :' , sum)
+
